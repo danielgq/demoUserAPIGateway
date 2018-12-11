d313e75277ef332fcc1a324b1406b3f39f58740f
danielgq <176838943@qq.com>
2018-12-11 10:54:00 +0800

    small change

--- middlewares/validateLogin.js
+++ middlewares/validateLogin.js
@@ -2,23 +2,15 @@ import fs from 'fs'
 
 const validateLogin = (req, res, next) => {
     const {adminName, adminPassword} = req.body;
-  /*  fs.readFile('config/validation.json','utf-8',function(err,data){
-        if(data){
-            const {validAdminName, validAdminPassword} = JSON.parse(data);*/
-            if (validAdminName === 'admin' && validAdminPassword === 'admin123') {
-                console.log('validation pass')
-                next()
-            } else {
-                console.log('validation failed')
-                return res.status(401).json({
-                    msg: 'invalid admin info!!!'
-                })
-            }
-    /*}else if(err){
-        console.log("error occurred when read the file")
-        console.log(err)
+    if (adminName === 'admin' && adminName === 'admin123') {
+        console.log('validation pass')
+        next()
+    } else {
+        console.log('validation failed')
+        return res.status(401).json({
+            msg: 'invalid admin info!!!'
+        })
     }
-})*/
 
 }
 
